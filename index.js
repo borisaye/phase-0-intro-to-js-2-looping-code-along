9d76a208f0681de2c6d2cf32a4f5c6f06f3b25ed
Borisaye <boris_aroche@hotmail.com>
2023-03-11 15:18:58 -0500

    add

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+let giftGivers = [];
+let event = ["surprise"];
+
+
+function writeCards(giftGivers){
+
+    let giftReply = [];
+    for (let i = 0; i < giftGivers.length; i++){
+        giftReply[i] = (`Thank you, ${giftGivers[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return giftReply;
+
+}
+
+let numCount = [];
+let newCount;
+
+function countDown(numCount) {
+
+    for (let countdown = 10; countdown !== -1; countdown--) {
+        console.log(countdown);
+        
+      }
+
+
+    return numCount;
+
+
+}
